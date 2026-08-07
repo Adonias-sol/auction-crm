2c7c248aab62b6e429d5d7d40976677b68b94fad
Adonias Solomon <adoniassolomon31@gmail.com>
2026-08-07 14:20:47 +0300

    Phase 1.1: Harden production security settings

--- auction_crm/settings.py
+++ auction_crm/settings.py
@@ -10,12 +10,21 @@ SECRET_KEY = config(
     default="django-insecure-$@vt=8b)un9e=m^zp0l97nnw0v#emzdw#!h3&rtrf!xcxutxu^"
 )
 
-DEBUG = config("DEBUG", default=True, cast=bool)
+DEBUG = config("DEBUG", default=False, cast=bool)
 
 ALLOWED_HOSTS = config(
     "ALLOWED_HOSTS",
-    default="127.0.0.1,localhost"
+    default="auction-crm-api.onrender.com"
 ).split(",")
+
+CSRF_TRUSTED_ORIGINS = config(
+    "CSRF_TRUSTED_ORIGINS",
+    default="https://auction-crm-frontend.onrender.com"
+).split(",")
+
+SESSION_COOKIE_SECURE = not DEBUG
+SESSION_COOKIE_HTTPONLY = True
+SECURE_SSL_REDIRECT = not DEBUG
 
 
 # Application definition
@@ -112,9 +121,9 @@ STATIC_ROOT = BASE_DIR / 'staticfiles'
 STORAGES = {'staticfiles': {'BACKEND': 'whitenoise.storage.CompressedManifestStaticFilesStorage'}}
 
 CORS_ALLOWED_ORIGINS = config(
-    'CORS_ALLOWED_ORIGINS',
-    default='http://localhost:5173,http://localhost:3000'
-).split(',')
+    "CORS_ALLOWED_ORIGINS",
+    default="https://auction-crm-frontend.onrender.com"
+).split(",")
 
 # Media files (keep local for now)
 MEDIA_URL = "/media/"

130fc2765e2ba875f6604f9d7a3b387ce8f3fd67
Adonias-sol <adoniassolomon31@gmail.com>
2026-08-20 20:25:17 +0300

    refix invoices and test git

--- auction_crm/settings.py
+++ auction_crm/settings.py
@@ -14,7 +14,7 @@ DEBUG = config("DEBUG", default=False, cast=bool)
 
 ALLOWED_HOSTS = config(
     "ALLOWED_HOSTS",
-    default="auction-crm-api.onrender.com"
+    default="localhost,127.0.0.1,auction-crm-api.onrender.com"
 ).split(",")
 
 CSRF_TRUSTED_ORIGINS = config(
